b58d286ee9cdc64c76a0d39de619cd07498b0f7b
afroozsamaei <afrooz.samaei@gmail.com>
2016-11-27 19:23:54 -0500

    Add files via upload

---
+++ Read_Scale_Values/Read_Scale_Values.ino
@@ -0,0 +1,75 @@
+//Take a reading from a scale and post it to Adafruit.IO
+
+#include "config.h"
+#include "HX711.h"
+
+#define calibration_factor 8500.0 //This value is different for different scales. 
+//It should be obtained by putting a known weight on the scale
+//and changing the calibration factor until the scale shows the right weight.
+
+//Defining the pins
+#define DOUT  16
+#define CLK  SCK
+
+HX711 scale(DOUT, CLK);
+
+//variable to hold the value from scale
+int scaleVal;
+int weight;
+
+// set up the 'weightVal' feed
+AdafruitIO_Feed *weightVal = io.feed("weightVal");
+
+
+void setup() {
+
+  // start the serial connection
+  Serial.begin(115200);
+
+  scale.set_scale(calibration_factor);
+  scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
+
+
+  // wait for serial monitor to open
+  while (! Serial);
+
+  Serial.print("Connecting to Adafruit IO");
+
+  // connect to io.adafruit.com
+  io.connect();
+
+  // wait for a connection
+  while (io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+
+}
+
+void loop() {
+
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  io.run();
+
+  //get value from scale
+  scaleVal = scale.get_units();
+  weight = scaleVal * 0.453592;
+
+  if (weight > 20) //check if someone is standing in the booth
+  {
+    // save weight to the 'weightVal' feed on Adafruit IO
+    weightVal->save(weight);
+
+  }
+
+  Serial.print("weight -> ");
+  Serial.println(weight);
+
+}
---
+++ Read_Ultrasonic_Values/Read_Ultrasonic_Values.ino
@@ -0,0 +1,68 @@
+//Take a reading from an Ultrasonic sensor and post it to Adafruit.IO
+
+#include "config.h"
+#include <NewPing.h>
+
+#define TRIGGER_PIN  14  // Feather pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     12  // Feather pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+
+int sensorVal;          //variable to hold the value from the sensor
+int height;
+int floorToTop = 197;   //Height of the booth
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+// set up the 'distVal' feed
+AdafruitIO_Feed *distVal = io.feed("distVal");
+
+void setup() {
+
+  // start the serial connection
+  Serial.begin(115200);
+
+  // wait for serial monitor to open
+  while (! Serial);
+
+  Serial.print("Connecting to Adafruit IO");
+
+  // connect to io.adafruit.com
+  io.connect();
+
+  // wait for a connection
+  while (io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+
+}
+
+void loop() {
+
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  io.run();
+
+  //getValue from Sensor
+  sensorVal = sonar.ping_cm();
+
+  if (sensorVal < 50 && sensorVal > 0) //check if someone is standing in the booth
+  {
+    height = floorToTop - sensorVal;
+    // save height to the 'distVal' feed on Adafruit IO
+    distVal->save(height);
+
+    //print the value to the serial monitor so you can check if its working
+    Serial.print("Height -> ");
+    Serial.println(height);
+
+  }
+
+}
